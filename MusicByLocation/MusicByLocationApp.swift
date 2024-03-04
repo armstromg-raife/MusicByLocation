805cde72efeb50521cae4abf101a7a109129423f
raife.armstrong <armstrong.raife@etoncollege.org.uk>
2024-03-04 18:52:23 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Raife Armstrong on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
